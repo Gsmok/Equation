94cc23e87adc468f9da6c8d86840e6ae92e56970
Ryuujin <Jakubowski@mail-site.cba.pl>
2016-12-18 00:11:30 +0100

    Constant implementation
    Implement Constant methods and set project to be following c++11 standard

--- defs/basics/constant.cpp
+++ defs/basics/constant.cpp
@@ -1,3 +1,47 @@
 #include "constant.h"
 
 using namespace solver;
+
+cConstant::cConstant(double _value)
+:value(_value)
+{}
+
+bool cConstant::isEqual(const iExpression& arg)const
+{
+    return (&arg==this)||arg.eval()==value;
+}
+
+bool cConstant::isEqual(const iExpression& arg)
+{
+    return ((cConstant*)this)->isEqual(arg);
+}
+
+double cConstant::eval()const
+{
+    return value;
+}
+
+double cConstant::eval()
+{
+    return ((cConstant*)this)->eval();
+}
+
+double cConstant::compare(const iExpression& arg)const
+{
+    return eval() - arg.eval();
+}
+
+double cConstant::compare(const iExpression& arg)
+{
+    return ((cConstant*)this)->compare(arg);
+}
+
+iExpression* cConstant::simplify()
+{
+    return nullptr;
+}
+
+bool cConstant::contains(const iExpression& arg)const
+{
+    return this == &arg;
+}
